9edb0ac7855497b4ca7d90c8d3087648bcd7b511
karishma-fariha <karishmafarihakathi10@gmail.com>
2025-10-12 23:46:24 +0600

    error fixed

--- src/Component/Loading/Loading.jsx
+++ src/Component/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className='pt-32 flex justify-center items-center'>
+        <div className='py-64 flex justify-center items-center'>
             <h1 className='text-8xl font-bold 
              text-[#632EE3]'>L
             <span className="loading loading-spinner loading-xl"></span>ading</h1>
